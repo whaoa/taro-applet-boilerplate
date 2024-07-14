c8840a74184a646fdc97479a40bc25f63d1d57fe
whaoa <whaoa.w@outlook.com>
2024-07-14 15:09:02 +0800

    feat: add stylelint support

---
+++ stylelint.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ['stylelint-config-standard-scss'],
+  rules: {
+    'alpha-value-notation': 'number',
+    'color-hex-length': null,
+    'color-function-notation': 'legacy',
+    'comment-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+        ignoreComments: ['endregion'],
+      },
+    ],
+    'custom-property-empty-line-before': null,
+    'property-no-unknown': [
+      true,
+      {
+        ignoreSelectors: [':export'],
+      },
+    ],
+    'no-duplicate-selectors': null,
+    'selector-not-notation': 'simple',
+    'selector-class-pattern': null,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['export', 'global'],
+      },
+    ],
+    'no-descending-specificity': null,
+    'scss/at-import-partial-extension': 'always',
+    'scss/double-slash-comment-empty-line-before': null,
+  },
+};
