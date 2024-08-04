2b011de3f655542cb35fb89747e95ff525dac5e4
whaoa <whaoa.w@outlook.com>
2024-08-04 17:23:55 +0800

    refactor: move taroify to nut-ui

---
+++ src/libs/ui/popup.ts
@@ -0,0 +1,7 @@
+import type { PopupProps as IPopupProps } from '@nutui/nutui-react-taro';
+
+import '@nutui/nutui-react-taro/dist/esm/Popup/style/style.css';
+
+export { Popup } from '@nutui/nutui-react-taro';
+
+export type PopupProps = Partial<IPopupProps>;
