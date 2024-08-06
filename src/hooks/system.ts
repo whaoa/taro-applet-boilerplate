7cf028b17e998b68c2885aeb38bf83516a7d60e6
whaoa <whaoa.w@outlook.com>
2024-08-06 11:49:02 +0800

    refactor: change the implementation of system-related hooks
    make it support multiple pages with different navigation bar configs

--- src/hooks/system.ts
+++ src/hooks/system.ts
@@ -1,9 +1,33 @@
-import { useSystemStore } from '@/stores/system/store';
+import { useEffect, useMemo } from 'react';
+
+import { IS_WEB_PLATFORM } from '@/constants/taro';
+import { getNavigationBoundingClientRect, getSafeAreaInsetsSync } from '@/libs/taro/system';
+
+let navigationLayoutCache: ReturnType<typeof getNavigationBoundingClientRect> | null = null;
 
 export function useNavigationLayout() {
-  return useSystemStore((state) => state.navigation);
+  const navigation = useMemo(() => {
+    if (IS_WEB_PLATFORM && navigationLayoutCache) {
+      return navigationLayoutCache;
+    }
+    return getNavigationBoundingClientRect();
+  }, []);
+
+  useEffect(() => {
+    navigationLayoutCache = navigation;
+  }, [navigation]);
+
+  return navigation;
 }
 
+let safeAreaInsetsCache: ReturnType<typeof getSafeAreaInsetsSync> | null = null;
+
 export function useSafeAreaInsets() {
-  return useSystemStore((state) => state.safeAreaInsets);
+  const insets = useMemo(() => (safeAreaInsetsCache || getSafeAreaInsetsSync()), []);
+
+  useEffect(() => {
+    safeAreaInsetsCache = insets;
+  }, [insets]);
+
+  return insets;
 }
